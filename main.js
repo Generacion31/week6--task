6be558f15cad9962ad6724886fda150a4484d7fe
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-10-02 19:33:13 -0300

    task

---
+++ main.js
@@ -0,0 +1,56 @@
+/***************************/
+/* Ejercicios con el DOM: */
+/**************************/
+
+const usuarios = [
+  {
+    nombre: 'Erik',
+    edad: 29,
+    correo: 'erik@academlo.com',
+    social: [
+      { nombre: 'facebook', url: 'facebook/erik' },
+      { nombre: 'twitter', url: 'twitter/erik' }
+    ],
+    genero: 'Male'
+  },
+  {
+    nombre: 'Georg',
+    edad: 33,
+    correo: 'georg@academlo.com',
+    social: [
+      { nombre: 'facebook', url: 'facebook/georg' },
+      { nombre: 'twitter', url: 'twitter/georg' }
+    ],
+    genero: 'Male'
+  },
+  {
+    nombre: 'Andrea',
+    edad: 42,
+    correo: 'andrea@hotmail.com',
+    social: [
+      { nombre: 'facebook', url: 'facebook/andrea' },
+      { nombre: 'twitter', url: 'twitter/andrea' }
+    ],
+    genero: 'Female'
+  },
+  {
+    nombre: 'Oscar',
+    edad: 31,
+    correo: 'oscar@academlo.com',
+    social: [
+      { nombre: 'facebook', url: 'facebook/oscar' },
+      { nombre: 'twitter', url: 'twiter/oscar' }
+    ],
+    genero: 'Male'
+  },
+  {
+    nombre: 'Daniela',
+    edad: 22,
+    correo: 'daniela@uaq.mx',
+    social: [
+      { nombre: 'facebook', url: 'facebook/andrea' },
+      { nombre: 'twitter', url: 'twitter/andrea' }
+    ],
+    genero: 'Female'
+  }
+]
